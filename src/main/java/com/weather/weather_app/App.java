d1d6c266a8a968423ce9efa160a42294ebda262b
yamsinator <BuiSteven@gmail.com>
2024-04-03 13:38:51 -0700

    First commit. Initializing first files

---
+++ src/main/java/com/weather/weather_app/App.java
@@ -0,0 +1,13 @@
+package com.weather.weather_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
